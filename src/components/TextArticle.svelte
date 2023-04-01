fe6612b827cef67c2913d2795e5d28fbfd6382aa
Viktor Holta <viktor.holta@outlook.com>
2023-04-01 17:52:28 +0200

    NEW: layout, about page, text article, and styling

---
+++ src/components/TextArticle.svelte
@@ -0,0 +1,16 @@
+<article>
+    <slot />
+</article>
+
+<style lang="scss">
+    @import "../variables.scss";
+    @import "../default.scss";
+
+    article {
+        margin: $medium-size 0;
+        padding: $medium-size;
+        border-radius: $medium-size;
+        background: $black;
+        color: $white;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,76 @@
+<div>
+    <header>
+        <h1>Viktor Holta</h1>
+        <nav>
+            <a href="/">About</a>
+            <a href="/projects">Projects</a>
+        </nav>
+    </header>
+    
+    <main>
+        <slot />
+    </main>
+    
+    <footer>
+        <h4>Viktor Holta <b>©</b> 2023</h4>
+    </footer>
+</div>
+
+<style lang="scss">
+    @import "../variables.scss";
+    @import "../default.scss";
+
+    div {
+        display: flex;
+        flex-direction: column;
+        margin: 0 auto;
+        max-width: 70rem;
+		min-height: 100vh;
+        color: $black;
+    }
+
+    header {
+        display: flex;
+        flex-direction: row;
+        gap: $medium-size;
+        padding: $medium-size;
+
+        h1 { margin: 0; }
+
+        nav {
+            flex-grow: 1;
+
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            gap: $small-size;
+
+            a {
+                align-self: center;
+
+                border: 3px solid $black;
+                padding: $small-size $medium-size;
+                border-radius: $medium-size;
+                color: $black;
+                font-weight: 600;
+                text-decoration: none;
+                transition: all 50ms ease-out;
+
+                &:hover {
+                    border-color: transparent;
+                    background: $black;
+                    color: #009245;
+                }
+            }
+        }
+    }
+
+    main {
+        padding: 0 $medium-size;
+        flex-grow: 1;
+    }
+
+    footer {
+        padding: $medium-size;
+    }
+</style>
